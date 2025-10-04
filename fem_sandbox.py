66c354bfbe343bb6f4cc6f142e52162adbd09d34
Gary Guzzo <garymguzzo@gmail.com>
2025-10-04 16:33:39 -0400

    Initial release of global stiffness matrix

--- fem_sandbox.py
+++ fem_sandbox.py
@@ -10,7 +10,7 @@ class FiniteElement:
         self.end = end
         self.n = n
     
-    def create_partition(self,start,end,n):
+    def create_random_partition(self,start,end,n):
         start = self.start
         end = self.end
         n = self.n
@@ -25,6 +25,17 @@ class FiniteElement:
         for i in range(len(bdrs)-1):
             partition.append((bdrs[i],bdrs[i+1]))
         return partition
+    
+    def create_fixed_partition(self):
+        start = 0
+        end = 1
+        n = 5
+
+        bdrs = np.linspace(start,end,n+1)
+        partition = []
+        for i in range(len(bdrs)-1):
+            partition.append((bdrs[i],bdrs[i+1]))
+        return partition     
         
     def visualize_interval_partitions(self, partitions, original_start, original_end, title="Interval Partitions"):
         fig, ax = plt.subplots(figsize=(10, 2))
@@ -63,32 +74,104 @@ class FiniteElement:
         return m*(x-start)
     
     def create_element_domain(self,interval,m=50):
-        return np.linspace(interval[0],interval[-1],m,endpoint=True)
+        return np.linspace(interval[0],interval[-1],m+1,endpoint=True)
+    
+    def derivative(self,phi,omega):
+        dy = np.gradient(phi,omega)
+        return dy[:-1]
 
     def create_shape_functions(self,partition):
         Phi = []
+        Omega = []
+        Dx = []
         
         sf1 = self.shape_function_1
         sf2 = self.shape_function_2
         for i in range(self.n):
             interval = partition[i]
             element_domain = self.create_element_domain(interval)
+            dx = element_domain[1]-element_domain[0]
             phi = []
             
-            phi[0] = [sf1(x,interval) for x in element_domain]
-            phi[1] = [sf2(x,interval) for x in element_domain]
+            phi.append([sf1(x,interval) for x in element_domain])
+            phi.append([sf2(x,interval) for x in element_domain])
             
-            Phi.append(phi)
+            Phi.append(np.array(phi))
+            Omega.append(element_domain)
+            Dx.append(dx)
+            
+        return [Phi,Omega,Dx]
+    
+    def plot_shape_functions(self,Phi,Omega):
+        colors = plt.cm.Set2(np.linspace(0,1,self.n))
+        for i in range(len(Phi[0])):
+            for j in range(self.n):
+                plt.plot(Omega[j],Phi[j][i,:],color=colors[j])
+        plt.show()
+    
+    def compute_stiffness_entry(self,phi_1,phi_2,omega,dx):
+        dphi_1 = self.derivative(phi_1,omega)
+        dphi_2 = self.derivative(phi_2,omega)
+        return np.dot(dphi_1,dphi_2)*dx
+    
+    def compute_stiffness_matrix(self,phi,omega,dx):
+        k = np.zeros((2,2))
+        for i in range(k.shape[0]):
+            for j in range(k.shape[1]):
+                k[i,j] = self.compute_stiffness_entry(phi[i,:],phi[j,:],omega,dx)
+        return k
+    
+    def compute_stiffness_matrices(self,Phi,Omega,Dx):
+        K_e = []
+        for i in range(self.n):
+            K_e.append(self.compute_stiffness_matrix(Phi[i],Omega[i],Dx[i]))
+        return K_e
+    
+    def assemble_global_stiffness_matrix(self,K_e):
+        K = np.zeros((self.n+1,self.n+1))
+        for i in range(self.n):
+            K[i:i+2, i:i+2] += K_e[i]
+        return K
+    
+def main():
+    start = 0
+    end = 1
+    n=5
+
+    fem = FiniteElement(start,end,n)
+    partition = fem.create_random_partition(start, end, n)
+    # partition = fem.create_fixed_partition()
+
+    [Phi,Omega,Dx] = fem.create_shape_functions(partition)
+    
+    fem.plot_shape_functions(Phi,Omega)
+
+    K_e = fem.compute_stiffness_matrices(Phi,Omega,Dx)
+    K = fem.assemble_global_stiffness_matrix(K_e)
+    
+    return K
+
+if __name__ == "__main__":
+    K = main()
+    
+    
+
         
-        return Phi
-            
-
-start = 0
-end = 1
-n = 8
-
-fem = FiniteElement(start,end,n)
-partition = fem.create_partition(start, end, n)
-
-Phi = fem.create_shape_functions(partition)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
